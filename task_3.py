22686727fe0021a17281d266e47c3a6f0f94091d
Mojiklo <motozudjika@gmail.com>
2024-10-23 21:48:28 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,13 +1,13 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
 
-obem = 1.44
-co_stronic = 100
-co_strok = 50
-co_simv = 25
-v1simvole = 4
+volume = 1.44
+number_of_pages = 100
+number_of_lines = 50
+number_of_characters = 25
+symbol_weight = 4
 
-kniga_bait = co_stronic * co_strok * co_simv * v1simvole
-kniga_mb = (kniga_bait) / (1024*1024)
+book_bait = number_of_pages * number_of_lines * number_of_characters * symbol_weight
+book_mb = (book_bait) / (1024*1024)
 
-col = round((obem / kniga_mb))
-print("Количество книг, помещающихся на дискету:", col)
+cound_books = round(volume / book_mb)
+print("Количество книг, помещающихся на дискету:", cound_books)

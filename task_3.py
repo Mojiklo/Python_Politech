338127d79ab8ad84001f7caf4fa231128ab77e72
Mojiklo <motozudjika@gmail.com>
2024-10-24 12:13:18 +0300

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -9,5 +9,5 @@ symbol_weight = 4
 book_byte = number_of_pages * number_of_lines * number_of_characters * symbol_weight
 book_mb = book_byte / (1024*1024)
 
-cound_books = int(volume / book_mb + 0.9999999)
-print("Количество книг, помещающихся на дискету:", cound_books)
+count_books = int(volume / book_mb + 0.9999999)
+print("Количество книг, помещающихся на дискету:", count_books)
